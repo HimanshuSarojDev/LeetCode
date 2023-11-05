ede55c0aed16620e0f026a638b3e1af81f516aee
himanshusaroj <himanshusaroj1997@gmail.com>
2023-11-05 11:39:25 +0530

    leetcode1

---
+++ Twosum.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        num_indices = {}  
+
+        for i in range(len(nums)):
+            num = nums[i]
+            complement = target - num
+            if complement in num_indices:
+                return [num_indices[complement], i]
+            num_indices[num] = i
+
+solution = Solution()
+nums = [2, 7, 11, 15]
+target = 9
+result = solution.twoSum(nums, target)
+if result:
+    print(result)  
